e01995314c4c1f1f49b3057cabdc47e202bc1fc5
wanya2002 <wanya2002@yandex.ru>
2025-03-08 12:12:27 +0300

    3

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from catalog.models import Dog, Breed
 
-# Register your models here.
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'breed')
+    list_filter = ('breed',)
+    search_fields = ('name',)
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
